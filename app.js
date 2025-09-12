6a03428e999978ca14995bf3aeeb2374d709137a
Mike Sordilla <sordilla1@gmail.com>
2025-09-12 19:09:50 +0800

    Simple Calculator

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hello, World! JS");
+function addNumber(x, y) {
+  console.log(`${x} + ${y} = ${x + y}`);
+}
+
+function subtractNumber(x, y) {
+  console.log(`${x} - ${y} = ${x - y}`);
+}
+
+function multiplyNumber(x, y) {
+  console.log(`${x} x ${y} = ${x * y}`);
+}
+
+function divisionNumber(x, y) {
+  console.log(`${x} / ${y} = ${x / y}`);
+}
+
+function averageNumber() {}
+
+addNumber(1, 2);
+subtractNumber(5, 3);
+multiplyNumber(5, 3);
+divisionNumber(10, 2);
